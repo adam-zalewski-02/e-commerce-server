7deeddb1b21164beaffbebef19930eb5795c65c8
Adam Zalewski <VisterPL23@gmail.com>
2023-06-20 18:58:56 +0200

    add ProductTranslation model and its relations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    public function translations() {
+        return $this->hasMany(ProductTranslation::class);
+    }
 }
