3e2ad1ba7127c6703dc7d6754d38a4ff88a11821
Adam Zalewski <VisterPL23@gmail.com>
2023-06-20 19:29:23 +0200

    add GetProducts in ProductController and GET route in api.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Modules\Products\Services\ProductService;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    private $_service;
+    public function __construct(ProductService $service)
+    {
+        $this->_service = $service;
+    }
+
+    public function getProducts(Request $request) {
+        $pages = $request->get("pages", 10);
+
+        if($pages == 0) {
+            return response()->json(['error' => 'No products found.'], 404);
+        }
+
+        return response()->json([
+            'data' => $this->_service->all($pages)
+        ], 200);
+    }
 }
