71618440cc6f38ac1e78e80a00734b7af21e0621
Adam Zalewski <VisterPL23@gmail.com>
2023-06-22 14:40:12 +0200

    add basic OrderService with rules

---
+++ app/Modules/Orders/Services/OrderService.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Modules\Orders\Services;
+
+use App\Models\Order;
+use App\Modules\Core\Services\Service;
+
+class OrderService extends Service {
+
+    protected $_rules = [
+        'user_id' => 'required|exists:users,id',
+        'status' => 'required|in:pending,processing,completed,cancelled',
+        'total_amount' => 'required|numeric|min:0',
+        'shipping_address' => 'required|string|max:255',
+        'billing_address' => 'required|string|max:255'
+    ];
+
+    public function __construct(Order $model)
+    {
+        parent::__construct($model);
+    }
+}
