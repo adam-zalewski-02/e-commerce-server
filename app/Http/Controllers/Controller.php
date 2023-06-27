65527eb3ee2094ed2cbbb58f5c7921bf5b98684d
Adam Zalewski <VisterPL23@gmail.com>
2023-06-27 20:02:24 +0200

    add protected response functions in Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,9 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected function sendOkResponse($data, $key = 'data', $statusCode = Response::HTTP_OK) {
+        return response()->json([
+            $key => $data
+        ], $statusCode);
+    }
+
+    protected function sendCreatedResponse($message, $data, $key = 'data', $statusCode = Response::HTTP_CREATED) {
+        return response()->json([
+            'message' => $message,
+            $key => $data
+        ], $statusCode);
+    }
+
+    protected function sendNotFoundResponse($message, $statusCode = Response::HTTP_NOT_FOUND) {
+        return response()->json([
+            'error' => $message
+        ], $statusCode);
+    }
+
+    protected function sendBadRequestResponse($message, $statusCode = Response::HTTP_BAD_REQUEST) {
+        return response()->json([
+            'error' => $message
+        ], $statusCode);
+    }
 }
